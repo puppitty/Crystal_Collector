8bff97d075b07875c82c423a2745a74799b0fb43
puppitty <33668867+puppitty@users.noreply.github.com>
2018-02-23 14:51:49 -0500

    Initial HTML file - outline for javascript some CSS

---
+++ assets/javascript/game.js
@@ -0,0 +1,18 @@
+// variables to be written back to HTML
+
+
+// variables for the game
+
+// Reset Game function
+
+
+// create random number from 19 to 120 for gameRandomNo
+
+// create random numbers for each of hte gems between 1 and 12, but all 4 different
+
+// On firt click of one of the gems, check to see if game is running, display to the DOM the value of gameRandomNo
+// show value of the GEm stone. Add value to playerScore, Check playerScore to gameRandomNo. If === player wins the game. If > player loses the game. else go back to allow another Gem click.
+
+//Update total score, wins, losses
+
+// Write answers to the DOM
